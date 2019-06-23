a21498f5423373d937ffa8df9e382fe040b57815
Yuke Wang <505008605@qq.com>
2019-06-23 14:51:42 -0700

    modify dataset

--- main.cpp
+++ main.cpp
@@ -21,23 +21,22 @@ int main()
 {
 	citationKNN CKNN;
 
-	// CKNN.readData("./cknn-dataset/01_elephant.csv");
+	CKNN.readData("./cknn-dataset/01_elephant.csv");
 	// CKNN.readData("./cknn-dataset/02_fox.csv");
 	// CKNN.readData("./cknn-dataset/03_tiger.csv");
 	// CKNN.readData("./cknn-dataset/04_musk1.csv");
 	// CKNN.readData("./cknn-dataset/05_musk2.csv");
 	// CKNN.readData("./cknn-dataset/06_Mutagenesis_atoms.csv");
-	// CKNN.readData("./cknn-dataset/07_Mutagenesis_chains.csv");
-	// CKNN.readData("./cknn-dataset/08_Mutagensis_bounds.csv");
+	// CKNN.readData("./cknn-dataset/07_Mutagensis_bounds.csv");
+	// CKNN.readData("./cknn-dataset/08_Mutagenesis_chains.csv");
 
 	hDistType = 0;
-	isOriginal = false;
+	// isOriginal = false;
 	int R = 2;  //2
 	int C = 4; 	//4
 	int max_grp = 0;
 
-
-	int start_s=clock();
+	int start_s=clock(), start_k, end_k, acc_k = 0;
 	
 	if(!isOriginal)
 		CKNN.grouping(1);
@@ -59,17 +58,22 @@ int main()
 	{
 		cout<<"Pkg: "<<i<<endl;
 		CKNN.clean(i); //clean all existing info about i
-		if(!isOriginal)
+		if(!isOriginal){
+			start_k=clock();
 			res.push_back(CKNN.predict(i, R, C));
+			end_k=clock();
+			acc_k += end_k - start_k;
+		}
 		else
 			res.push_back(CKNN.ori_predict(i, R, C));
 	}
 	int stop_s=clock();
 
-	cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << endl;
-	
-	for(int i = 0; i < res.size(); i++)
-		cout<< "[ "<< i << "] " << res[i] << std::endl;
+	cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000  << " ms" << endl;
+	cout << "Kernel time: " << acc_k/double(CLOCKS_PER_SEC)*1000  << " ms" << endl;
+
+	// for(int i = 0; i < res.size(); i++)
+	// 	cout<< "[ "<< i << "] " << res[i] << std::endl;
 
 	int accurateItem = 0;
 	for(int i = 0; i < res.size(); i++)
@@ -78,16 +82,16 @@ int main()
 			accurateItem++;
 	}
 
-	cout<<(double)accurateItem / (double)res.size()<<endl;
-	long long SUM = countDistComp + countDistComp2 + countDistComp3 + countDistComp4 + countDistComp5;
-	cout<<"dist:"<<countDistComp<<" "<<countDistComp / (double)SUM <<endl;
-	cout<<"dist2:"<<countDistComp2<<" "<<countDistComp2 / (double)SUM <<endl;
-	cout<<"dist3:"<<countDistComp3<<" "<<countDistComp3 / (double)SUM <<endl;
-	cout<<"dist4:"<<countDistComp4<<" "<<countDistComp4 / (double)SUM <<endl;
-	cout<<"dist5:"<<countDistComp5<<" "<<countDistComp5 / (double)SUM <<endl;
-	cout<<"total:"<<SUM<<endl;
-	cout<<"grp amount:"<<CKNN.numGroups<<endl;
-	cout<<"avg dist:"<<distSum / distCount<<endl;
+	cout<< "Accuracy: " << (double)accurateItem / (double)res.size() <<endl;
+	// long long SUM = countDistComp + countDistComp2 + countDistComp3 + countDistComp4 + countDistComp5;
+	// cout<<"dist:"<<countDistComp<<" "<<countDistComp / (double)SUM <<endl;
+	// cout<<"dist2:"<<countDistComp2<<" "<<countDistComp2 / (double)SUM <<endl;
+	// cout<<"dist3:"<<countDistComp3<<" "<<countDistComp3 / (double)SUM <<endl;
+	// cout<<"dist4:"<<countDistComp4<<" "<<countDistComp4 / (double)SUM <<endl;
+	// cout<<"dist5:"<<countDistComp5<<" "<<countDistComp5 / (double)SUM <<endl;
+	// cout<<"total:"<<SUM<<endl;
+	// cout<<"grp amount:"<<CKNN.numGroups<<endl;
+	// cout<<"avg dist:"<<distSum / distCount<<endl;
 	
 	return 0;
 }
